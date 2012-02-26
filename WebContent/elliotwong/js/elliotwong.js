949e5fc4574e01a2aa19beab8de4c43117995dcb
Jeff Wong <awole20@gmail.com>
2012-02-25 19:04:30 -0800

    initial commit of site

---
+++ WebContent/elliotwong/js/elliotwong.js
@@ -0,0 +1,8 @@
+function navigatePage(showdiv){
+	var divs = ['gallery','about','contact'];
+	
+	for(var i = 0; i < divs.length; i++){
+		document.getElementById(divs[i]).style.display = 'none';
+	}
+	document.getElementById(showdiv).style.display = '';
+}
